373141cad45f26facc55c0413aea8ea2cfc77590
Tiago Dos Santos Cordeiro <86391368+Tiagosancor@users.noreply.github.com>
2022-01-27 09:14:49 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using JogoDeRpg.src.Entities;
+using static System.Console;
+
+
+namespace JogoDeRpg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hero Knight = new Knight("Arus", 23, "Knigth");
+           Wizard wizard = new Wizard("Jennica", 23, "White Wizard");
+           
+            WriteLine(Knight.Attack());
+            WriteLine(wizard.Attack());
+            
+        }
+
+        
+    }
+
+}
---
+++ src/Entities/Wizard.cs
@@ -0,0 +1,23 @@
+namespace JogoDeRpg.src.Entities
+{
+    public class Wizard : Hero
+    {
+        public Wizard(string Name, int Level, string HeroType):base(Name, Level, HeroType)
+        {
+            this.Name = Name;
+            this.Level = Level;
+            this.HeroType = HeroType;
+        }
+
+
+        public override string Attack()
+        {
+            return this.Name + " Lançou Magia";
+        }
+        
+        public string Attack(int Bonus)
+        {
+            return this.Name + " Lançou Magia com bonus de ataque de " + Bonus;
+        }
+    }
+}
